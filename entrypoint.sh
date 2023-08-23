c0fa131bbda7f8eb2420b1049146c532a2e7b3fc
sreelakshmi2805 <msreelakshmi18@gmail.com>
2023-08-23 13:40:03 +0530

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,19 @@
 #!/bin/sh -l
 
-echo "Hello $1"
-time=$(date)
-echo "time=$time" >> $GITHUB_OUTPUT
+# echo "Hello $1"
+# time=$(date)
+# echo "time=$time" >> $GITHUB_OUTPUT
+
+export CI_COMMIT_BRANCH=$1
+export INT_ENV_HOST_IP=$2
+export DEV_ENV_HOST_IP=$3
+
+if [ "$CI_COMMIT_BRANCH" == "dev" ]; then
+  echo "Setting Host IP to $DEV_ENV_HOST_IP for dev branch"
+  export ENV_HOST_IP=$DEV_ENV_HOST_IP
+elif [ "$CI_COMMIT_BRANCH" == "int" ]; then
+  echo "Setting Host IP to $INT_ENV_HOST_IP for int branch"
+fi
+
+# Export the environment variables
+echo "ENV_HOST_IP=$ENV_HOST_IP" >> $GITHUB_ENV
