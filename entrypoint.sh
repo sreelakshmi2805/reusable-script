e475c45937ca9530430d758203833bff7782a639
sreelakshmi2805 <msreelakshmi18@gmail.com>
2023-08-23 14:21:17 +0530

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,19 +4,19 @@
 # time=$(date)
 # echo "time=$time" >> $GITHUB_OUTPUT
 
-export CI_COMMIT_BRANCH=$1
-export INT_ENV_HOST_IP=$2
-export DEV_ENV_HOST_IP=$3
+export CI_COMMIT_BRANCH="$CI_COMMIT_BRANCH"
+export INT_ENV_HOST_IP="$INT_ENV_HOST_IP"
+export DEV_ENV_HOST_IP="$DEV_ENV_HOST_IP"
 
 if [ "$CI_COMMIT_BRANCH" == "master" ]; then
-  echo "Setting Host IP to $DEV_ENV_HOST_IP for dev branch"
-  export ENV_HOST_IP=$DEV_ENV_HOST_IP
+  echo "Setting Host IP to "$DEV_ENV_HOST_IP" for dev branch"
+  export ENV_HOST_IP="$DEV_ENV_HOST_IP"
 elif [ "$CI_COMMIT_BRANCH" == "int" ]; then
-  echo "Setting Host IP to $INT_ENV_HOST_IP for int branch"
-  export ENV_HOST_IP=$INT_ENV_HOST_IP
+  echo "Setting Host IP to "$INT_ENV_HOST_IP" for int branch"
+  export ENV_HOST_IP="$INT_ENV_HOST_IP"
 fi
 
-echo "branch is  $1"
+echo "branch is '$CI_COMMIT_BRANCH'"
 echo "Debug: ENV_HOST_IP=$ENV_HOST_IP"
 # Export the environment variables
 echo "ENV_HOST_IP=$ENV_HOST_IP" >> $GITHUB_ENV
