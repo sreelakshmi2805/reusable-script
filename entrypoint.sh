dad1461d343009d875803c86029ecb0cb0bf5119
sreelakshmi2805 <msreelakshmi18@gmail.com>
2023-08-23 14:47:56 +0530

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,22 +4,20 @@
 # time=$(date)
 # echo "time=$time" >> $GITHUB_OUTPUT
 
-export CI_COMMIT_BRANCH=$1
-export INT_ENV_HOST_IP=$2
-export DEV_ENV_HOST_IP=$3
+export DEV_ENV_HOST_IP=$1
 
-if [ "$CI_COMMIT_BRANCH" = "master" ]; then
-  echo "Setting Host IP to $DEV_ENV_HOST_IP for master branch"
-  export ENV_HOST_IP="$DEV_ENV_HOST_IP"
-elif [ "$CI_COMMIT_BRANCH" = "int" ]; then
-  echo "Setting Host IP to $INT_ENV_HOST_IP for int branch"
-  export ENV_HOST_IP="$INT_ENV_HOST_IP"
-fi
+# if [ "$CI_COMMIT_BRANCH" = "master" ]; then
+#   echo "Setting Host IP to $DEV_ENV_HOST_IP for master branch"
+#   export ENV_HOST_IP="$DEV_ENV_HOST_IP"
+# elif [ "$CI_COMMIT_BRANCH" = "int" ]; then
+#   echo "Setting Host IP to $INT_ENV_HOST_IP for int branch"
+#   export ENV_HOST_IP="$INT_ENV_HOST_IP"
+# fi
 
-echo "Branch is $CI_COMMIT_BRANCH"
-echo "Debug: ENV_HOST_IP=$ENV_HOST_IP"
+echo "Branch is $1"
+# echo "Debug: ENV_HOST_IP=$ENV_HOST_IP"
 
-# Export the environment variables
-echo "ENV_HOST_IP=$ENV_HOST_IP" >> $GITHUB_ENV
+# # Export the environment variables
+# echo "ENV_HOST_IP=$ENV_HOST_IP" >> $GITHUB_ENV
 
-echo "Debug: ENV_HOST_IP=$ENV_HOST_IP"
+# echo "Debug: ENV_HOST_IP=$ENV_HOST_IP"
