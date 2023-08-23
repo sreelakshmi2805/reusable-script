ccc5be50db63698c2b708f536d592fbfa3c50077
sreelakshmi2805 <msreelakshmi18@gmail.com>
2023-08-23 20:15:11 +0530

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,49 +1,40 @@
 #!/bin/sh -l
+CI_COMMIT_BRANCH="$CI_COMMIT_BRANCH"
+DEV_ENV_HOST_IP="$DEV_ENV_HOST_IP"
+DEV_MONGODB__DATABASE_NAME="$MONGODB__DATABASE_NAME"
+DEV_AZURESTORAGE__CONTAINER_NAME="$AZURESTORAGE__CONTAINER_NAME"
+INT_ENV_HOST_IP="$INT_ENV_HOST_IP"
+INT_MONGODB__DATABASE_NAME="$INT_MONGODB__DATABASE_NAME"
+INT_AZURESTORAGE__CONTAINER_NAME="$INT_AZURESTORAGE__CONTAINER_NAME"
+PROD_ENV_HOST_IP="$PROD_ENV_HOST_IP"
+PROD_MONGODB__DATABASE_NAME="$PROD_MONGODB__DATABASE_NAME"
+PROD_AZURESTORAGE__CONTAINER_NAME="$PROD_AZURESTORAGE__CONTAINER_NAME"
+STAGING_ENV_HOST_IP="$STAGING_ENV_HOST_IP"
+STAGING_MONGODB__DATABASE_NAME="$STAGING_MONGODB__DATABASE_NAME"
+STAGING_AZURESTORAGE__CONTAINER_NAME="$STAGING_AZURESTORAGE__CONTAINER_NAME"
 
-
-# eval dynamic_variable_name="\$${dev_env_host_ip}"
-# echo "ip is $dynamic_variable_name"
-
-dev_env_host_ip="$dev_env_host_ip"
-echo "ip is $dev_env_host_ip"
-
-# while [[ $# -gt 0 ]]; do
-#   case "$1" in
-#     *=*)
-#       varname="${1%%=*}"
-#       value="${1#*=}"
-#       export "$varname=$value"
-#       ;;
-#   esac
-#   shift
-# done
-
-# dev_env_host_ip="$dev_env_host_ip"
-# echo "ip is $dev_env_host_ip" 
-
-# DEV_IP="dev_env_host_ip"
-# DEV_ENV_IP="${inputs[$DEV_IP]}"
-# echo "ip is $DEV_ENV_IP"
-
-
-
-# time=$(date)
-# echo "time=$time" >> $GITHUB_OUTPUT
-
-# export DEV_ENV_HOST_IP=$1
-
-# if [ "$CI_COMMIT_BRANCH" = "master" ]; then
-#   echo "Setting Host IP to $DEV_ENV_HOST_IP for master branch"
-#   export ENV_HOST_IP="$DEV_ENV_HOST_IP"
-# elif [ "$CI_COMMIT_BRANCH" = "int" ]; then
-#   echo "Setting Host IP to $INT_ENV_HOST_IP for int branch"
-#   export ENV_HOST_IP="$INT_ENV_HOST_IP"
-# fi
-
-# echo "ip is $1"
-# echo "Debug: ENV_HOST_IP=$ENV_HOST_IP"
-
-# # Export the environment variables
-echo "dev_env_host_ip=$dev_env_host_ip" >> $GITHUB_ENV
-
-# echo "Debug: ENV_HOST_IP=$ENV_HOST_IP"
+# export CI_COMMIT_BRANCH=$CI_COMMIT_BRANCH
+if [ "$CI_COMMIT_BRANCH" == "dev" ]; then
+    echo "Setting Host IP to $DEV_ENV_HOST_IP for dev branch"
+    export ENV_HOST_IP= $DEV_ENV_HOST_IP          
+    export MONGODB__DATABASE_NAME= $DEV_MONGODB__DATABASE_NAME         
+    export AZURESTORAGE__CONTAINER_NAME= $DEV_AZURESTORAGE__CONTAINER_NAME
+elif [ "$CI_COMMIT_BRANCH" == "int" ]; then
+    echo "Setting Host IP to  $INT_ENV_HOST_IPfor int branch"
+    export ENV_HOST_IP= $INT_ENV_HOST_IP
+    export MONGODB__DATABASE_NAME= $INT_MONGODB__DATABASE_NAME
+    export AZURESTORAGE__CONTAINER_NAME= $INT_AZURESTORAGE__CONTAINER_NAME
+elif [ "$CI_COMMIT_BRANCH" == "master" ]; then
+    echo "Setting Host IP to  $PROD_ENV_HOST_IPfor master branch"
+    export ENV_HOST_IP= $PROD_ENV_HOST_IP
+    export MONGODB__DATABASE_NAME= $PROD_MONGODB__DATABASE_NAME
+    export AZURESTORAGE__CONTAINER_NAME= $PROD_AZURESTORAGE__CONTAINER_NAME
+elif [ "$CI_COMMIT_BRANCH" == "staging" ]; then
+    echo "Setting Host IP to  $STAGING_ENV_HOST_IPfor staging branch"
+    export ENV_HOST_IP= $STAGING_ENV_HOST_IP
+    export MONGODB__DATABASE_NAME= $STAGING_MONGODB__DATABASE_NAME
+    export AZURESTORAGE__CONTAINER_NAME= $STAGING_AZURESTORAGE__CONTAINER_NAME
+fi
+echo "ENV_HOST_IP=$ENV_HOST_IP" >> $GITHUB_ENV
+echo "MONGODB__DATABASE_NAME=$MONGODB__DATABASE_NAME" >> $GITHUB_ENV
+echo "AZURESTORAGE__CONTAINER_NAME=$AZURESTORAGE__CONTAINER_NAME" >> $GITHUB_ENV
